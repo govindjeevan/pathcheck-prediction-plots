9447c8182c26cdd694c9ed06eb1ed8cf99e07fd0
Govind Jeevan <govindjeevan7@gmail.com>
2021-06-23 11:42:54 +0530

    style changes

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 function toggleDesc() {
-    console.log("Hi")
     var element = document.getElementsByClassName("desc")[0];
     element.classList.toggle("active");
-    if (element.style.maxHeight) {
-        element.style.maxHeight = null;
-      } else {
-        element.style.maxHeight = element.scrollHeight + "px";
-      } 
+
   }
